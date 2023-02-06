1dfd12250a741d9844b4fb28e34a12ae16b143da
arthurbreis <arthur_dos_reis@hotmail.com>
2023-02-06 19:33:57 -0300

    iniciando curso 3

---
+++ Go/src/projeto03.alura/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "Index", nil)
+}
+
+type Produto struct {
+	Nome, Descricao string
+}
