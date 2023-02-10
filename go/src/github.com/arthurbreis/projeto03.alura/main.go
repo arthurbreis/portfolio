e3f2889d9c144bfaebf484a576a67213fbf8aac4
arthurbreis <arthur_dos_reis@hotmail.com>
2023-02-10 16:28:42 -0300

    atualizando

--- go/src/github.com/arthurbreis/projeto03.alura/main.go
+++ go/src/github.com/arthurbreis/projeto03.alura/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"net/http"
+
+	_ "github.com/lib/pq"
 )
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
@@ -13,9 +15,18 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{"Camiseta", "Azul, bem bonita", 39, 5},
+		{"Tenis", "Confortável", 89, 3},
+		{"Fone", "Muito bom", 59, 2},
+		{"Produto novo", "Muito legal", 1.99, 1},
+	}
+
+	templates.ExecuteTemplate(w, "Index", produtos)
 }
 
 type Produto struct {
 	Nome, Descricao string
+	Preco           float64
+	Quantidade      int
 }
