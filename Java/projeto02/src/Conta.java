b2c7207d2e907caad10b75924430741e80871bd2
arthurbreis <arthur_dos_reis@hotmail.com>
2023-03-22 20:22:25 -0300

    atualizando

--- Java/projeto02/src/Conta.java
+++ Java/projeto02/src/Conta.java
@@ -1,6 +1,56 @@
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;  
+    private double saldo;
+    private int agencia;
+    private int numero;
+    private Cliente titular;
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public Cliente getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
 }
--- Java/projeto02/src/CriaConta.java
+++ Java/projeto02/src/CriaConta.java
@@ -1,17 +1,39 @@
 public class CriaConta {
     public static void main(String[] args) {
-        
-       Conta primeiraConta = new Conta();
-       primeiraConta.saldo = 200;
-       primeiraConta.agencia = 1234;
-       primeiraConta.numero = 12;
-       primeiraConta.titular = "Arthur";
+        //DEFINE CLIENTE
+        Cliente arthur = new Cliente();
+        arthur.setNome("Arthur");
+        arthur.setCpf ("1234567890");
+        //DEFINE CONTA 
+        Conta primeiraConta = new Conta();
+        primeiraConta.deposita(200);
+        primeiraConta.setAgencia(1234);
+        primeiraConta.setNumero(12);
+        //SETA TITULAR
+        primeiraConta.setTitular(arthur);
 
-       System.out.println(primeiraConta.saldo);
+        System.out.println(primeiraConta.getTitular().getNome());
 
-       primeiraConta.saldo += 23;
+        System.out.println(primeiraConta.getSaldo());
 
-       System.out.println(primeiraConta.saldo);
+        primeiraConta.deposita(23);
 
-    } 
+        System.out.println(primeiraConta.getSaldo());
+
+        primeiraConta.saca(100);
+
+        System.out.println(primeiraConta.getSaldo());
+
+        Conta segundaConta = new Conta();
+        segundaConta.deposita(200);
+
+        if (segundaConta.transfere(150, primeiraConta)) {
+            System.out.println("Transferência realizada com sucesso!");
+        } else {
+            System.out.println("Transferência não realizada!");
+        }
+
+        System.out.println(primeiraConta.getSaldo());
+        System.out.println(segundaConta.getSaldo());
+    }
 }
