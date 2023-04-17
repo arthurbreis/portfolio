dc5444c6513f1f602dd859ab6fa62e9cfd5e475a
arthurbreis <arthur_dos_reis@hotmail.com>
2023-04-17 18:45:47 -0300

    atualizando

--- Java/projeto05/src/Main.java
+++ Java/projeto05/src/Main.java
@@ -1,9 +1,12 @@
 import models.*;
+
+import java.util.ArrayList;
+
 import calculos.*;
 
 public class Main {
     public static void main(String[] args) {
-       Filme filme01 = new Filme();
+        Filme filme01 = new Filme();
         filme01.setNome("Top gun");
         filme01.setAnoLancamento(2022);
         filme01.setDuracao(120);
@@ -12,6 +15,16 @@ public class Main {
 
         filme01.avalia(10);
         filme01.avalia(20);
+
+        Filme filme02 = new Filme();
+        filme02.setNome("The DaVinci Code");
+        filme02.setAnoLancamento(2015);
+        filme02.setDuracao(120);
+
+        filme02.fichaTecnica();
+
+        filme02.avalia(10);
+        filme02.avalia(20);
 
         System.out.println(filme01.getTotalAvaliacoes());
 
@@ -24,7 +37,6 @@ public class Main {
         serie01.setTemporadas(5);
         serie01.setMinPorEps(60);
 
-        
         serie01.fichaTecnica();
 
         CalculadoraTempo calc = new CalculadoraTempo();
@@ -34,7 +46,14 @@ public class Main {
 
         FiltroRecomendacao filtro = new FiltroRecomendacao();
         filtro.filtra(filme01);
-        
+
+        ArrayList<Filme> filmes = new ArrayList<>();
+        filmes.add(filme01);
+        filmes.add(filme02);
+        System.out.println("Tamanho da lista: " + filmes.size());
+        System.out.println("Primero da lista: " + filmes.get(0).getNome());
+
+
 
     }
 }
