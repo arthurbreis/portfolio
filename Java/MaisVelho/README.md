fd50804410799d8cc207d11ff5d2160d6b1c2c44
arthurbreis <arthur_dos_reis@hotmail.com>
2023-07-07 19:45:54 -0300

    Exercício Resolvido

---
+++ Java/MaisVelho/README.md
@@ -0,0 +1,3 @@
+Fazer um programa para ler um conjunto de nomes de pessoas e suas respectivas idades. Os nomes
+devem ser armazenados em um vetor, e as idades em um outro vetor. Depois, mostrar na tela o nome
+da pessoa mais velha.
---
+++ Java/MaisVelho/src/entities/Pessoa.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(){
+    }
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+}
